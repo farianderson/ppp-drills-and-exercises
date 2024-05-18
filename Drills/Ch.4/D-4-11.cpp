25892987ee0b44ddbbfefe6fd8119b698ab7d262
Fari Anderson <farianderson101@gmail.com>
2024-05-19 03:26:55 +0330

    adding D-4-11.cpp

--- Drills/Ch.4/D-4-11.cpp
+++ Drills/Ch.4/D-4-11.cpp
@@ -1,27 +1,26 @@
-// 10. Keep all the values entered (converted into meters) in a vector. At the
-//     End, write out those values.
+// 11. Before writing out the values from the vector, sort then (that'll make
+//     them come out in increasing order).
 
 // Note: while inputting, it's not necessary to put a white-space between the number and unit.
 // for example: inputting "20m" will be no different than inputting "20 m". I guess that's becuase we've used
 // a pattern like: cin >> doulbe >> string.
 
-#include <iostream>
-#include <string>
-#include <vector>
+//this time using std_lib_facilities :|
+#include "/home/fari/Downloads/std_lib_facilities.h"
 
 // these constants are defined because they may be used more than once
 const char* msg_input = "Enter a number followed by a unit, like 22 m or 12.5 cm\nThe acceptable units are : m, cm, in, ft";
 const char* msg_exit="program exit...";
 const char exit_char='|'; 
-const std::vector<std::string> accepted_units = {"cm" , "m" , "in" , "ft"};
-const std::vector<double> unit_factors = {.01, 1, .0254, 12*.0254};
+const vector<std::string> accepted_units = {"cm" , "m" , "in" , "ft"};
+const vector<double> unit_factors = {.01, 1, .0254, 12*.0254};
 
 int main() 
 {
 
     std::cout << msg_input << std::endl;
 
-    std::vector<double> values;
+    vector<double> values;
 
     double input_double, value;
     std::string unit;
@@ -53,12 +52,11 @@ int main()
         if (std::cin >> input_char)
             if (input_char == exit_char)
             {
-                double min = values[0],max = values[0],sum = 0;
+                sort(values);
+                double min = values[0],max = values[values.size()-1],sum = 0;
                 std::cout << std::endl << "the values are: " << std::endl;
                 for(double num:values)
                 {
-                    if(num < min) min = num;
-                    if(num > max) max = num;
                     sum += num;
                     std::cout << num << 'm' << std::endl;
                 }
