f766deabba10a2d2525cd7a15e8741f00a7bb385
Fari Anderson <farianderson101@gmail.com>
2024-07-14 02:27:58 +0330

    hmmm

---
+++ Exercises/Ch.05/E-05-02.cpp
@@ -0,0 +1,33 @@
+// 2. The following program takes in a temperature value in Celsius and 
+//    converts it to Kelvin. This code has many errors in it. Find the errors,
+//    list them, and correct the code.
+
+//    double ctok(double c)   // converts Celsius to Kelvin                      
+//    {
+//      int k = c + 273.25;
+//      return int
+//    }
+//    
+//    int main()
+//    {
+//      double c = 0;         // declare input variable
+//      cin >> d;             // retrieve temperature to input variable         
+//      double k = ctok("c"); // convert temperature
+//      Cout << k << '/n' ;   // print out temperature                      
+//    }
+
+#include "../../std_lib_facilities.h"
+
+double ctok(double c)   // converts Celsius to Kelvin                      
+{
+  double k = c + 273.25;
+  return k;
+}
+
+int main()
+{
+  double c = 0;         // declare input variable
+  cin >> c;             // retrieve temperature to input variable         
+  double k = ctok(c);   // convert temperature
+  cout << k << '\n' ;   // print out temperature                      
+}
